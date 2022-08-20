b8912b3893f4fda0df212a21133bfe28274b5a6d
mask <soulmask.mr@gmail.com>
2022-08-20 13:44:18 +0000

    单元测试

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,6 +1,10 @@
 package lru
 
-import "testing"
+import (
+	"container/list"
+	"reflect"
+	"testing"
+)
 
 type String string
 
@@ -8,13 +12,106 @@ func (d String) Len() int {
 	return len(d)
 }
 
-func TestGet(t *testing.T) {
-	lru := New(int64(0), nil)
-	lru.Add("key1", String("1234"))
-	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
-		t.Fatalf("cache hit key1=1234 failed")
+func TestCache_RemoveOldest(t *testing.T) {
+	type fields struct {
+		maxBytes  int64
+		nbytes    int64
+		ll        *list.List
+		cache     map[string]*list.Element
+		OnEvicted func(key string, value Value)
 	}
-	if _, ok := lru.Get("key2"); ok {
-		t.Fatalf("cache miss key2 failed")
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Cache{
+				maxBytes:  tt.fields.maxBytes,
+				nbytes:    tt.fields.nbytes,
+				ll:        tt.fields.ll,
+				cache:     tt.fields.cache,
+				OnEvicted: tt.fields.OnEvicted,
+			}
+			c.RemoveOldest()
+		})
+	}
+}
+
+func TestCache_Add(t *testing.T) {
+	type fields struct {
+		maxBytes  int64
+		nbytes    int64
+		ll        *list.List
+		cache     map[string]*list.Element
+		OnEvicted func(key string, value Value)
+	}
+	type args struct {
+		key   string
+		value Value
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+		{name: "testkey1", fields: fields(*New(10, nil)), args: args{key: "testkey1", value: String("11222")}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Cache{
+				maxBytes:  tt.fields.maxBytes,
+				nbytes:    tt.fields.nbytes,
+				ll:        tt.fields.ll,
+				cache:     tt.fields.cache,
+				OnEvicted: tt.fields.OnEvicted,
+			}
+			c.Add(tt.args.key, tt.args.value)
+		})
+	}
+}
+
+func TestCache_Get(t *testing.T) {
+	type fields struct {
+		maxBytes  int64
+		nbytes    int64
+		ll        *list.List
+		cache     map[string]*list.Element
+		OnEvicted func(key string, value Value)
+	}
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		wantValue Value
+		wantOk    bool
+	}{
+		// TODO: Add test cases.
+		{name: "undefind", fields: fields(*New(10, nil)), args: args{key: "testkey1"}, wantValue: nil, wantOk: false},
+		{name: "getOne", fields: fields{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Cache{
+				maxBytes:  tt.fields.maxBytes,
+				nbytes:    tt.fields.nbytes,
+				ll:        tt.fields.ll,
+				cache:     tt.fields.cache,
+				OnEvicted: tt.fields.OnEvicted,
+			}
+			gotValue, gotOk := c.Get(tt.args.key)
+			if !reflect.DeepEqual(gotValue, tt.wantValue) {
+				t.Errorf("Cache.Get() gotValue = %v, want %v", gotValue, tt.wantValue)
+			}
+			if gotOk != tt.wantOk {
+				t.Errorf("Cache.Get() gotOk = %v, want %v", gotOk, tt.wantOk)
+			}
+		})
 	}
 }
