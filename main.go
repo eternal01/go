e459502758acf4dcbd4c19bd148e4d990ec72223
mask <soulmask.mr@gmail.com>
2022-10-18 14:15:56 +0000

    update print code

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 
 // handler echoes r.URL.Path
 func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	_,_ = fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
 }
 
 // handler echoes r.URL.Header
 func helloHandler(w http.ResponseWriter, req *http.Request) {
 	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		_,_ = fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
 }
